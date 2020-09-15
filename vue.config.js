37e88b2b08cb06c7d14d712e9ea597a1fca16460
hyj111 <1069234622@qq.com>
2020-09-15 22:35:13 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 module.exports = {
+   
   // 基本路径
-  publicPath: process.env.NODE_ENV === 'production' ? '' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-admin-web/' : './',
   // 输出文件目录
   outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
   // eslint-loader 是否在保存的时候检查
